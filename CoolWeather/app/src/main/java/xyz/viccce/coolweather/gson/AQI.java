0987b08b9a1b63c66cbdf60f006d80e7296192ce
viccce <295759631@qq.com>
2018-10-16 16:34:04 +0800

    GSON

---
+++ CoolWeather/app/src/main/java/xyz/viccce/coolweather/gson/AQI.java
@@ -0,0 +1,12 @@
+package xyz.viccce.coolweather.gson;
+
+public class AQI {
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
